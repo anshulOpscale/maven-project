fcd84f22b77522a32b0be90946a7ff9be81057ba
ANSHUL JAIN <anshul.jain@opscale.io>
2019-07-10 16:51:28 +0530

    xyz

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,10 +9,7 @@ public class Greeter {
   /**
    * This is a constructor.
    */
-  public Greeter() {
-
-  }
-
+ 
   public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
